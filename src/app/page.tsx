89b22a494dd82a9e791ce27636ede1a61c50289a
Erick Araújo <araujoerick.dev@gmail.com>
2025-04-25 22:34:45 -0300

    ✨ feat: add initial Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,84 @@
 import Image from "next/image";
+import Header from "./components/header";
+import { FaLinkedin, FaGithubSquare, FaStar } from "react-icons/fa";
+import Link from "next/link";
 
-export default function Home() {
+const Home = () => {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <>
+      <Header />
+      <div className="grid h-[calc(100vh-166px)] w-full grid-cols-2 items-center justify-center bg-lime-300">
+        <div className="grid h-full grid-rows-2 text-black">
+          <div className="border-r-4 border-b-4 bg-lime-100 p-16">
+            <p className="text-9xl font-bold uppercase">Erick</p>
+            <p className="text-9xl font-bold uppercase">Araujo</p>
+          </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          <div className="border-r-4 bg-lime-50 p-16">
+            <h2 className="text-6xl font-bold text-black">
+              Desenvolvedor Frontend
+            </h2>
+            <button className="btn-primary">Projetos</button>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+
+        <div className="flex h-full items-center justify-center">
+          <div className="group relative pb-8">
+            <Image
+              src="/perfil-base.png"
+              alt="Foto de perfil"
+              width={343}
+              height={439}
+              className="shadow-[30px_40px_0px_rgba(0,0,0,0)] transition-all duration-300 group-hover:drop-shadow-[30px_40px_0px_rgba(0,0,0)]"
+              priority
+            />
+
+            <Link
+              href="https://www.linkedin.com/in/araujoerick09/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaLinkedin
+                size={32}
+                className="absolute right-4 bottom-[72px] cursor-pointer text-lime-300 transition-colors duration-300 hover:text-lime-400"
+              />
+            </Link>
+            <Link
+              href="https://github.com/araujoerick"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaGithubSquare
+                size={32}
+                className="absolute right-20 bottom-8 cursor-pointer text-lime-300 hover:text-lime-400"
+              />
+            </Link>
+          </div>
+        </div>
+
+        {/* <div className="fixed inset-0 z-[5]">
           <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+            src="/bg-shapes.svg"
+            alt="bg shapes"
+            fill
+            className="object-cover opacity-30"
+            priority
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        </div> */}
+      </div>
+      <div className="flex h-full max-h-20 w-full items-center justify-center gap-4 border-t-4 bg-white px-8 py-4 text-4xl font-bold">
+        <span>PORTIFOLIO</span>
+        <FaStar />
+        <span>PORTIFOLIO</span>
+        <FaStar />
+        <span>PORTIFOLIO</span>
+        <FaStar />
+        <span>PORTIFOLIO</span>
+        <FaStar />
+        <span>PORTIFOLIO</span>
+      </div>
+    </>
   );
-}
+};
+
+export default Home;
