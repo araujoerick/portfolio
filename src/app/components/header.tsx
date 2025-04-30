0a194f95e85ba07e5b3b5130c8060ec0dd267216
Erick Araújo <araujoerick.dev@gmail.com>
2025-04-30 01:26:09 -0300

    📱 add responsive breakpoints to Header

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -2,12 +2,14 @@ import MenuButton from "./header-menu-button";
 
 const Header = () => {
   return (
-    <header className="flex w-full items-center justify-between border-b-4 border-black bg-white px-8 py-3">
-      <span className="btn-primary px-4 text-3xl leading-8 font-extrabold whitespace-nowrap text-black uppercase select-none">
-        E
-      </span>
+    <header className="w-full border-b-4 border-black bg-white px-8 py-3">
+      <div className="container mx-auto flex items-center justify-between">
+        <span className="btn-primary px-4 text-3xl leading-8 font-extrabold whitespace-nowrap text-black uppercase select-none">
+          E
+        </span>
 
-      <MenuButton />
+        <MenuButton />
+      </div>
     </header>
   );
 };
