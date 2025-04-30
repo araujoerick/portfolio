d16d9d8ec66654e5267350ae5a829a63aea83f8a
Erick Araújo <araujoerick.dev@gmail.com>
2025-04-29 22:48:55 -0300

    ✨ feat: add bento grid to Home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
+      {/* <body className={`${inter.className} antialiased`}>{children}</body> */}
+      <body className={`font-mono antialiased`}>{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,190 @@
 import Image from "next/image";
 import Header from "./components/header";
-import { FaLinkedin, FaGithubSquare, FaStar } from "react-icons/fa";
 import Link from "next/link";
+import { FaGithub, FaLinkedinIn } from "react-icons/fa6";
 
 const Home = () => {
   return (
     <>
       <Header />
-      <main className="grid h-[calc(100vh-166px)] w-full grid-cols-2 items-center justify-center bg-lime-300 text-black">
-        <div className="grid h-full grid-rows-[30%,70%] text-black">
-          <div className="flex items-center border-r-4 border-b-4 bg-lime-100 p-10">
-            <h2 className="text-[clamp(2.5rem,4vw,3.75rem)] font-semibold text-black">
-              Desenvolvedor
-              <br />
-              Front-end
-            </h2>
-          </div>
 
-          <div className="flex flex-col items-start justify-center border-r-4 bg-lime-50 p-10 xl:gap-4">
-            <p className="mb-4 text-[clamp(1rem,1.5vw,1.25rem)] leading-relaxed font-semibold text-balance">
-              Desenvolvedor Front-end focado em construir aplicações web
-              modernas e performáticas. Sou graduado em Sistemas de Informação e
-              atualmente estou cursando uma pós-graduação em Desenvolvimento
-              Fullstack. Possuo experiência com React e Next.js utilizando
-              TypeScript e JavaScript, além de tecnologias como Node.js, Prisma,
-              APIs REST, Tailwind CSS e outras ferramentas do ecossistema web.
-            </p>
-            <p className="text-[clamp(1rem,1.5vw,1.25rem)] leading-relaxed font-semibold text-balance">
-              Minhas experiências anteriores me permitiram desenvolver
-              habilidades essenciais como comunicação, trabalho em equipe e
-              resolução de problemas. Sou apaixonado por criar interfaces
-              intuitivas e responsivas, sempre buscando as melhores práticas de
-              acessibilidade, usabilidade e design.
-            </p>
-          </div>
+      <main className="container mx-auto grid h-[calc(100vh-82px)] w-full grid-cols-4 grid-rows-3 items-center justify-center gap-5 py-12">
+        {/* NAME DIV */}
+        <div className="neo-border neo-shadow col-span-2 flex h-full items-center justify-center bg-lime-300 p-4">
+          <h1 className="text-[clamp(2.5rem,4vw,4.5rem)] font-bold uppercase">
+            Erick Araujo
+          </h1>
+        </div>
+        {/* TITLE DIV */}
+        <div className="neo-border neo-shadow col-span-2 flex h-full items-center justify-center p-4">
+          <h2 className="text-[clamp(1.5rem,4vw,2.5rem)] font-semibold text-nowrap text-black uppercase">
+            Desenvolvedor<span className="text-lime-400"> Front-end</span>
+          </h2>
         </div>
 
-        <div className="grid h-full w-full grid-rows-[70%,30%] gap-8">
-          <div className="flex h-full w-full items-center justify-center">
-            <div className="group relative p-4 pb-8">
-            <Image
-              src="/perfil-base.png"
-              alt="Foto de perfil"
-              width={343}
-              height={439}
-              className="shadow-[30px_40px_0px_rgba(0,0,0,0)] transition-all duration-300 group-hover:drop-shadow-[30px_40px_0px_rgba(0,0,0)]"
-              priority
-            />
+        {/* ABOUT DIV */}
+        <div className="neo-border neo-shadow col-span-2 row-span-2 flex h-full items-center justify-center p-8">
+          <p className="mb-4 text-[clamp(1rem,1.5vw,1.25rem)] leading-relaxed text-balance text-black">
+            Desenvolvedor Front-end focado em construir aplicações web modernas
+            e performáticas. Sou graduado em Sistemas de Informação e atualmente
+            estou cursando uma pós-graduação em Desenvolvimento Fullstack.
+            Possuo experiência com React e Next.js utilizando TypeScript e
+            JavaScript, além de tecnologias como Node.js, Prisma, APIs REST,
+            Tailwind CSS e outras ferramentas do ecossistema web.
+          </p>
+        </div>
 
-            <Link
-              href="https://www.linkedin.com/in/araujoerick09/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FaLinkedin
-                size={32}
+        {/* PROFILE DIV */}
+        <div className="row-span-3 flex h-full items-center justify-center">
+          <div className="flex h-full w-full flex-col items-center justify-center gap-4">
+            <div className="group relative pb-8">
+              <Image
+                src="/perfil-base.png"
+                alt="Foto de perfil"
+                width={343}
+                height={439}
+                className=""
+                priority
+              />
+
+              {/* <Link
+                href="https://www.linkedin.com/in/araujoerick09/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaLinkedin
+                  size={32}
                   className="absolute right-5 bottom-[78px] cursor-pointer text-lime-300 transition-colors duration-300 hover:text-lime-400"
-              />
-            </Link>
+                />
+              </Link>
+              <Link
+                href="https://github.com/araujoerick"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaGithubSquare
+                  size={32}
+                  className="absolute right-[74px] bottom-[46px] cursor-pointer text-lime-300 hover:text-lime-400"
+                />
+              </Link>
+              <Link
+                href="/docs/erick-araujo-cv.pdf"
+                target="_blank"
+                rel="noopener noreferrer"
+                download
+              >
+                <RiFileDownloadFill
+                  size={32}
+                  className="absolute right-[122px] bottom-4 cursor-pointer text-lime-300 hover:text-lime-400"
+                />
+              </Link> */}
+            </div>
+
             <Link
-              href="https://github.com/araujoerick"
+              href="/docs/erick-araujo-cv.pdf"
               target="_blank"
               rel="noopener noreferrer"
+              download
             >
-              <FaGithubSquare
-                size={32}
-                className="absolute right-20 bottom-8 cursor-pointer text-lime-300 hover:text-lime-400"
-              />
+              <button className="neo-border neo-shadow-sm bg-lime-300 p-4 text-black uppercase transition-colors duration-300 hover:bg-lime-400">
+                Download CV
+              </button>
             </Link>
           </div>
         </div>
 
-          <div className="flex h-full w-full items-center justify-center border-t-4 bg-lime-200 p-4">
-            <h1 className="text-[clamp(2.5rem,4vw,4.5rem)] font-bold uppercase">
-              Erick Araujo
-            </h1>
-          </div>
-      </div>
-      </main>
+        {/* SKILLS DIV */}
+        <div className="neo-border neo-shadow row-span-2 h-full p-8">
+          <h3 className="text-[clamp(1rem,4vw,2rem)] font-semibold text-black uppercase">
+            Habilidades
+          </h3>
+          <ul>
+            <li>
+              <span className="h-2 w-2"></span>
+              <span className="text-[clamp(1rem,1.5vw,1.25rem)] text-black">
+                React.js
+              </span>
+            </li>
+            <li>
+              <span className="text-[clamp(1rem,1.5vw,1.25rem)] text-black">
+                Next.js
+              </span>
+            </li>
+            <li>
+              <span className="text-[clamp(1rem,1.5vw,1.25rem)] text-black">
+                TypeScript
+              </span>
+            </li>
+            <li>
+              <span className="text-[clamp(1rem,1.5vw,1.25rem)] text-black">
+                JavaScript
+              </span>
+            </li>
+            <li>
+              <span className="text-[clamp(1rem,1.5vw,1.25rem)] text-black">
+                Tailwind CSS
+              </span>
+            </li>
+            <li>
+              <span className="text-[clamp(1rem,1.5vw,1.25rem)] text-black">
+                Node.js
+              </span>
+            </li>
+          </ul>
+        </div>
 
-      <div className="flex h-full max-h-20 w-full items-center justify-center gap-4 overflow-hidden border-t-4 bg-white px-8 py-4 text-4xl font-bold">
-        <span>PORTIFOLIO</span>
-        <FaStar />
-        <span>PORTIFOLIO</span>
-        <FaStar />
-        <span>PORTIFOLIO</span>
-        <FaStar />
-        <span>PORTIFOLIO</span>
-        <FaStar />
-        <span>PORTIFOLIO</span>
-      </div>
+        {/* STATS DIV */}
+        <div className="neo-border neo-shadow col-span-2 h-full p-8">
+          <h3 className="text-[clamp(1rem,4vw,2rem)] font-semibold text-black uppercase">
+            Estatísticas
+          </h3>
+          <ul>
+            <li>
+              <span className="text-[clamp(1rem,1.5vw,1.25rem)] text-black">
+                2+ Anos de experiência
+              </span>
+            </li>
+            <li>
+              <span className="text-[clamp(1rem,1.5vw,1.25rem)] text-black">
+                10+ Projetos desenvolvidos
+              </span>
+            </li>
+          </ul>
+        </div>
+
+        {/* CV DIV */}
+        {/* <div className="neo-border neo-shadow flex h-full items-center justify-center p-8">
+          
+        </div> */}
+
+        {/* SOCIAL DIV */}
+        <div className="neo-border neo-shadow h-full bg-lime-300 p-8">
+          <ul className="flex h-full items-center justify-center gap-4">
+            <li>
+              <Link
+                href="https://www.linkedin.com/in/araujoerick09/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <div className="neo-border neo-shadow-sm bg-white p-4">
+                  <FaLinkedinIn size={24} />
+                </div>
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="https://www.github.com/araujoerick"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <div className="neo-border neo-shadow-sm bg-white p-4">
+                  <FaGithub size={24} />
+                </div>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </main>
     </>
   );
 };
