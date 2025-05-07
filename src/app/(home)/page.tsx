cf0fcb45859e5a79b7605051c05bff1c1b475446
Erick Araújo <araujoerick.dev@gmail.com>
2025-05-07 02:40:37 -0300

    :truck: chore: move Home page to home folder

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,11 @@
+import BentoGrid from "./components/bento-grid";
+
+const Home = () => {
+  return (
+    <main className="min-h-[calc(100vh-82px)] w-full px-8 py-12 select-none">
+      <BentoGrid />
+    </main>
+  );
+};
+
+export default Home;
