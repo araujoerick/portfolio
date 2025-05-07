40b31e0773d64aee7fdba44845cec69778023fd4
Erick Araújo <araujoerick.dev@gmail.com>
2025-05-07 02:36:27 -0300

    ✨ feat: add BentoCard component

---
+++ src/app/(home)/components/bento-card.tsx
@@ -0,0 +1,23 @@
+import { cn } from "@/app/lib/utils";
+import React from "react";
+
+const BentoCard = ({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) => {
+  return (
+    <div
+      className={cn(
+        "bento-transition neo-border neo-shadow flex h-full items-center justify-center rounded-2xl",
+        className,
+      )}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default BentoCard;
