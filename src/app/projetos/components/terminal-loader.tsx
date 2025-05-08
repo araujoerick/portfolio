48331f575466106379be9cfb5a2e49c111a80682
Erick Araújo <araujoerick.dev@gmail.com>
2025-05-08 01:04:39 -0300

    ✨ feat: add TerminalLoader component to last project card

---
+++ src/app/projetos/components/terminal-loader.tsx
@@ -0,0 +1,71 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
+const TerminalLoader = () => {
+  const [visible, setVisible] = useState(true);
+  const [showCursor, setShowCursor] = useState(true);
+
+  useEffect(() => {
+    const cursorInterval = setInterval(() => {
+      setShowCursor((prev) => !prev);
+    }, 500);
+
+    return () => clearInterval(cursorInterval);
+  }, []);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setVisible((prev) => !prev);
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const asciiName = [
+    "███████╗██████╗ ██╗ ██████╗██╗  ██╗",
+    "██╔════╝██╔══██╗██║██╔════╝██║ ██╔╝",
+    "█████╗  ██████╔╝██║██║     █████╔╝ ",
+    "██╔══╝  ██╔══██╗██║██║     ██╔═██╗ ",
+    "███████╗██║  ██║██║╚██████╗██║  ██╗",
+    "╚══════╝╚═╝  ╚═╝╚═╝ ╚═════╝╚═╝  ╚═╝",
+  ];
+
+  return (
+    <div className="relative flex h-full w-full items-center overflow-hidden bg-neutral-900 p-2 font-mono text-sm text-lime-300 shadow-md">
+      <div className="absolute top-0 right-0 left-0 flex h-6 items-center justify-between rounded-t bg-neutral-800 px-2 text-xs text-neutral-200">
+        <div>Terminal</div>
+        <div className="flex space-x-1">
+          <div className="h-2.5 w-2.5 rounded-full bg-red-600" />
+          <div className="h-2.5 w-2.5 rounded-full bg-yellow-400" />
+          <div className="h-2.5 w-2.5 rounded-full bg-lime-600" />
+        </div>
+      </div>
+
+      <div className="mt-6 pt-2">
+        <div className="flex flex-col gap-2">
+          <span className="text-lime-300">$ mais projetos em breve...</span>
+          <span className="text-lime-300">$ loading system...</span>
+          <span className="text-lime-300">$ initialize_graphics.exe</span>
+          <span className="text-lime-300">$ render_name --style=pixelart</span>
+
+          {visible && (
+            <pre className="overflow-hidden text-lime-500">
+              {asciiName.map((line, index) => (
+                <div key={index} className="leading-none">
+                  {line}
+                </div>
+              ))}
+            </pre>
+          )}
+          <div className="flex">
+            <span className="text-lime-300">$ </span>
+            {showCursor && <span className="ml-1 h-4 w-2 bg-lime-300"></span>}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TerminalLoader;
