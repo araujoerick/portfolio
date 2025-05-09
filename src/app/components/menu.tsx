d911879ef91feb63e3a39d59fe8e7881e36b3ccb
Erick Araújo <araujoerick.dev@gmail.com>
2025-05-09 11:55:38 -0300

    :dizzy: remove button animation

--- src/app/components/menu.tsx
+++ src/app/components/menu.tsx
@@ -151,7 +151,7 @@ const Menu = () => {
                 rel="noopener noreferrer"
                 download
               >
-                <button className="item-transition-down btn-primary flex w-full cursor-pointer items-center justify-center gap-2 bg-lime-300 p-4 text-xl font-bold text-black uppercase">
+                <button className="btn-primary flex w-full cursor-pointer items-center justify-center gap-2 bg-lime-300 p-4 text-xl font-bold text-black uppercase">
                   <FaDownload size={16} />
                   Download CV
                 </button>
