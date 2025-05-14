a6a96c7ce4c6780d4531f1c395d256b1f46fce3b
Erick Araújo <araujoerick.dev@gmail.com>
2025-05-14 20:55:37 -0300

    💫 feat: add gsap animation with split type in title component

--- src/app/components/section-title.tsx
+++ src/app/components/section-title.tsx
@@ -1,10 +1,51 @@
-import React from "react";
+"use client";
 
-const SectionTitle = ({ children }: { children: string }) => {
+import React, { useEffect, useRef } from "react";
+import gsap from "gsap";
+import SplitType from "split-type";
+
+interface SectionTitleProps {
+  children: string;
+}
+
+const SectionTitle: React.FC<SectionTitleProps> = ({ children }) => {
+  const titleRef = useRef<HTMLHeadingElement>(null);
+
+  useEffect(() => {
+    if (titleRef.current) {
+      const split = new SplitType(titleRef.current, {
+        types: "chars",
+      });
+
+      gsap.from(split.chars, {
+        y: 100,
+        opacity: 0,
+        delay: 0.5,
+        duration: 0.5,
+        stagger: 0.05,
+        ease: "power2.out",
+      });
+
+      gsap.to(split.chars, {
+        y: 0,
+        opacity: 1,
+        delay: 1,
+        duration: 0.5,
+        stagger: 0.05,
+        ease: "power2.out",
+      });
+    }
+  }, []);
+
   return (
-    <h1 className="text-[clamp(2.25rem,4vw,3rem)] font-semibold text-black uppercase">
-      {children}
-    </h1>
+    <div>
+      <h1
+        ref={titleRef}
+        className="title-clip-path text-[clamp(2.25rem,4vw,3rem)] font-semibold text-black uppercase transition-transform duration-500"
+      >
+        {children}
+      </h1>
+    </div>
   );
 };
 
