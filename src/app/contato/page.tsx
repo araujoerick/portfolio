42f1ca0d3aafcc3de086a0be4ede9da1b75a4e9f
Erick Araújo <araujoerick.dev@gmail.com>
2025-05-17 19:06:59 -0300

    💄 feat: change padding at mobile breakpoints

--- src/app/contato/page.tsx
+++ src/app/contato/page.tsx
@@ -3,7 +3,7 @@ import ContactCard from "./components/contact-card";
 
 const Contato = () => {
   return (
-    <section className="min-h-[calc(100vh-82px)] w-full bg-lime-200 p-4 select-none min-[430px]:p-8">
+    <section className="min-h-[calc(100vh-82px)] w-full bg-lime-200 px-4 py-8 select-none min-[430px]:p-8">
       <div className="container mx-auto flex flex-col items-center justify-center gap-5">
         <SectionTitle>Contato</SectionTitle>
 
