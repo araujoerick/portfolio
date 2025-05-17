fe898ba2968bc99411e113071ff961454cd66153
Erick Araújo <araujoerick.dev@gmail.com>
2025-05-17 18:29:42 -0300

    ✨ feat: add useAnimated to ContactCard

--- src/app/contato/components/contact-card.tsx
+++ src/app/contato/components/contact-card.tsx
@@ -1,36 +1,21 @@
 "use client";
 
-import React, { useRef } from "react";
+import React from "react";
 import ContactInfo from "./contact-info";
 import { socialLinks } from "@/app/data/socialLinks";
 import Link from "next/link";
 import ContactForm from "./contact-form";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
+import { useAnimatedCards } from "@/app/hooks/use-animated-cards";
 
 gsap.registerPlugin(useGSAP);
 
 const ContactCard = () => {
-  const contactRef = useRef<HTMLDivElement>(null);
-
-  useGSAP(() => {
-    gsap.set(contactRef.current, { y: 50, opacity: 0 });
-
-    gsap.to(contactRef.current, {
-      y: 0,
-      opacity: 1,
-      delay: 0.3,
-      duration: 0.5,
-      ease: "power2.inOut",
-      immediateRender: false,
-    });
-  }, []);
+  useAnimatedCards();
 
   return (
-    <div
-      ref={contactRef}
-      className="initial-card card-transition bento-transition neo-border neo-shadow grid w-full max-w-[900px] grid-cols-1 gap-8 bg-white p-4 pb-6 min-[430px]:p-8 md:grid-cols-2"
-    >
+    <div className="initial-card neo-border neo-shadow grid w-full max-w-[900px] grid-cols-1 gap-8 bg-white p-4 pb-6 min-[430px]:p-8 md:grid-cols-2">
       <div className="flex flex-col gap-8">
         <div className="space-y-4">
           <h2 className="text-2xl font-semibold text-black">
