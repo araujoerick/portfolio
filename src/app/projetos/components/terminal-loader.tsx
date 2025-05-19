c05b011c29c19919aa0de588615bfae67fcda08d
Erick Araújo <araujoerick.dev@gmail.com>
2025-05-19 20:02:21 -0300

    ♻ refactor: change terminal animation to GSAP

--- src/app/projetos/components/terminal-loader.tsx
+++ src/app/projetos/components/terminal-loader.tsx
@@ -1,26 +1,12 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { useRef } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 
 const TerminalLoader = () => {
-  const [visible, setVisible] = useState(true);
-  const [showCursor, setShowCursor] = useState(true);
-
-  useEffect(() => {
-    const cursorInterval = setInterval(() => {
-      setShowCursor((prev) => !prev);
-    }, 500);
-
-    return () => clearInterval(cursorInterval);
-  }, []);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setVisible((prev) => !prev);
-    }, 5000);
-
-    return () => clearInterval(interval);
-  }, []);
+  const asciiRef = useRef<HTMLPreElement | null>(null);
+  const cursorRef = useRef<HTMLSpanElement | null>(null);
 
   const asciiName = [
     "██████╗█████╗ ██╗ █████╗██╗ ██╗",
@@ -30,6 +16,29 @@ const TerminalLoader = () => {
     "██████╗██║ ██║██║╚█████╗██║ ██╗",
     "╚═════╝╚═╝ ╚═╝╚═╝ ╚════╝╚═╝ ╚═╝",
   ];
+
+  useGSAP(() => {
+    if (cursorRef.current) {
+      gsap.to(cursorRef.current, {
+        opacity: 0,
+        repeat: -1,
+        yoyo: true,
+        duration: 0.5,
+        ease: "power1.inOut",
+      });
+    }
+
+    if (asciiRef.current) {
+      gsap.to(asciiRef.current, {
+        display: "none",
+        duration: 1,
+        repeat: -1,
+        yoyo: true,
+        ease: "power1.inOut",
+        repeatDelay: 3,
+      });
+    }
+  }, []);
 
   return (
     <div className="relative flex h-full min-h-[280px] w-full items-center overflow-hidden bg-neutral-900 p-2 font-mono text-sm text-lime-300 shadow-md">
@@ -49,18 +58,20 @@ const TerminalLoader = () => {
           <span className="text-lime-300">$ initialize_graphics.exe</span>
           <span className="text-lime-300">$ render_name --style=pixelart</span>
 
-          {visible && (
-            <pre className="overflow-hidden text-lime-500">
-              {asciiName.map((line, index) => (
-                <div key={index} className="leading-none">
-                  {line}
-                </div>
-              ))}
-            </pre>
-          )}
-          <div className="flex">
+          <pre ref={asciiRef} className="overflow-hidden text-lime-500">
+            {asciiName.map((line, index) => (
+              <div key={index} className="leading-none">
+                {line}
+              </div>
+            ))}
+          </pre>
+
+          <div className="flex items-center">
             <span className="text-lime-300">$ </span>
-            {showCursor && <span className="ml-1 h-4 w-2 bg-lime-300"></span>}
+            <span
+              ref={cursorRef}
+              className="ml-1 inline-block h-4 w-2 bg-lime-300"
+            ></span>
           </div>
         </div>
       </div>
