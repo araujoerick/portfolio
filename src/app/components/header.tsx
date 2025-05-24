c6ebf78a35f45940fcdc3f6f64da48c5f955020e
Erick Araújo <araujoerick.dev@gmail.com>
2025-05-24 01:29:56 -0300

    :wheelchair: feat: add ARIA attributes to improve accessibility

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 interface HeaderProps {
   isMenuOpen: boolean;
   toggleMenu: () => void;
@@ -5,14 +7,20 @@ interface HeaderProps {
 
 const Header = ({ isMenuOpen, toggleMenu }: HeaderProps) => {
   return (
-    <header className="w-full border-b-4 border-black bg-white px-8 py-3">
+    <header className="w-full border-b-4 border-black bg-white px-8 py-3 select-none">
       <div className="container mx-auto flex items-center justify-between">
-        <span className="btn-primary px-4 text-3xl leading-8 font-extrabold whitespace-nowrap text-black uppercase select-none">
+        <Link
+          href="/"
+          className="btn-primary px-4 text-3xl leading-8 font-extrabold whitespace-nowrap text-black uppercase"
+          aria-label="Ir para a página inicial"
+        >
           E
-        </span>
+        </Link>
 
         <button
           onClick={toggleMenu}
+          aria-label={isMenuOpen ? "Fechar menu" : "Abrir menu"}
+          aria-expanded={isMenuOpen}
           className="btn-primary flex cursor-pointer flex-col items-end justify-center gap-1 px-[13px] py-3.5"
         >
           <div
