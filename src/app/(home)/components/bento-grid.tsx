a6337204866f9eb13243cb5412d1c691f061feb4
Erick Araújo <araujoerick.dev@gmail.com>
2025-05-24 01:32:26 -0300

    ♿ feat: add ARIA attributes and screen reader only to improve accessibility

--- src/app/(home)/components/bento-grid.tsx
+++ src/app/(home)/components/bento-grid.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import Link from "next/link";
 import BentoCard from "./bento-card";
 import { FaGithub, FaLinkedinIn } from "react-icons/fa6";
 import Image from "next/image";
@@ -19,6 +18,7 @@ const BentoGrid = () => {
     <div className="container mx-auto grid grid-cols-1 items-center justify-center gap-5 min-[580px]:grid-cols-2 lg:grid-cols-4">
       {/* NAME DIV */}
       <BentoCard className="bg-lime-300 p-4 min-[580px]:col-span-2">
+        <h2 className="sr-only">Nome</h2>
         <h1 className="text-[clamp(2.25rem,4vw,4.5rem)] font-bold text-black uppercase">
           Erick Araujo
         </h1>
@@ -26,6 +26,7 @@ const BentoGrid = () => {
 
       {/* TITLE DIV */}
       <BentoCard className="p-4 min-[580px]:col-span-2">
+        <h2 className="sr-only">Profissão</h2>
         <h2 className="text-[clamp(1.25rem,3vw,2.5rem)] font-semibold text-nowrap text-black uppercase">
           Desenvolvedor<span className="text-lime-400"> Front-end</span>
         </h2>
@@ -33,6 +34,7 @@ const BentoGrid = () => {
 
       {/* ABOUT DIV */}
       <BentoCard className="row-span-2 flex-col gap-4 p-8 min-[580px]:col-span-2">
+        <h2 className="sr-only">Sobre mim</h2>
         <p className="text-[clamp(1rem,1.5vw,1.25rem)] leading-relaxed text-balance text-black">
           Desenvolvedor focado na criação de aplicações web modernas e
           performáticas, utilizando React e Next.js com JavaScript e TypeScript.
@@ -49,7 +51,8 @@ const BentoGrid = () => {
       </BentoCard>
 
       {/* PROFILE DIV */}
-      <BentoCard className="row-span-2 flex h-full flex-col items-center justify-between gap-4 overflow-hidden rounded-2xl bg-neutral-200 pb-14 lg:row-span-3">
+      <BentoCard className="row-span-2 flex h-full flex-col items-center justify-between gap-4 overflow-hidden rounded-2xl bg-neutral-200 pb-12 lg:row-span-3">
+        <h2 className="sr-only">Foto de perfil e currículo</h2>
         <Image
           src="/perfil-base.png"
           alt="Foto de perfil"
@@ -69,6 +72,7 @@ const BentoGrid = () => {
 
       {/* STATS DIV */}
       <BentoCard className="justify-around gap-4 px-4 py-8 min-[580px]:col-span-2 min-[580px]:p-8">
+        <h2 className="sr-only">Estatísticas</h2>
         <div className="flex flex-col items-center justify-center gap-2">
           <p className="text-4xl font-bold text-lime-300">10+</p>
           <p className="text-[clamp(.875rem,1.5vw,1.25rem)] text-black">
@@ -91,28 +95,31 @@ const BentoGrid = () => {
 
       {/* SOCIAL DIV */}
       <BentoCard className="bg-lime-300 p-8 min-[580px]:max-lg:row-start-6">
+        <h2 className="sr-only">Links sociais</h2>
         <ul className="flex h-full items-center justify-center gap-4">
           <li>
-            <Link
+            <a
               href="https://www.linkedin.com/in/araujoerick09/"
               target="_blank"
               rel="noopener noreferrer"
+              aria-label="Ir para o perfil de Erick Araujo no LinkedIn"
             >
               <div className="btn-primary bg-white p-4">
                 <FaLinkedinIn size={24} />
               </div>
-            </Link>
+            </a>
           </li>
           <li>
-            <Link
+            <a
               href="https://www.github.com/araujoerick"
               target="_blank"
               rel="noopener noreferrer"
+              aria-label="Ir para o perfil de Erick Araujo no Github"
             >
               <div className="btn-primary bg-white p-4">
                 <FaGithub size={24} />
               </div>
-            </Link>
+            </a>
           </li>
         </ul>
       </BentoCard>
